f31667e9d8d5ed6caa5743e21564f85cbd67cef3
Caroline Viana <carolborgesviana@gmail.com>
2023-02-06 15:39:11 -0300

    Cursos-Alura

---
+++ JavaScript - tipos, variáveis e funções/type-string.js
@@ -0,0 +1,34 @@
+const texto1 = "Olá, mundo!";
+const texto2 = 'Olá, mundo!';
+const senha = "senhaSuperSegura456!";
+const stringDeNumeros = "34567";
+
+const citacao = 'O Léo disse "Oi!"';
+console.log(citacao)
+
+// concatenação (+)
+
+const citacao2 = "Meu nome é: ";
+const meuNome = "Leonardo";
+
+console.log(citacao2 + meuNome)
+
+const cifrao = '\u0024'
+const aMaiusculo = '\u0041'
+const tique = '\u2705'
+const hiragana = '\u3041'
+
+console.log(cifrao)
+console.log(aMaiusculo)
+console.log(tique)
+console.log(hiragana)
+
+const cidade = "belo horizonte";
+const input = "Belo Horizonte";
+
+const inputMinusculo = input.toLowerCase();
+
+console.log(cidade === inputMinusculo); // true
+
+
+
